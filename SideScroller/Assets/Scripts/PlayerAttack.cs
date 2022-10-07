7b26a3342bc2a80482067a6ed919a058a3859b89
fianali <fontasia10@gmail.com>
2022-10-07 14:47:47 -0600

    created animation clip for attack

--- SideScroller/Assets/Scripts/PlayerAttack.cs
+++ SideScroller/Assets/Scripts/PlayerAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetInputs();
+    }
+
+    void GetInputs()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+                
+        }
     }
 }
